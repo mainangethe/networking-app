1a53d15c7c4ccff74d501662acfe65711aa64f9e
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-04-14 06:03:16 +0300

    Refactor navigation links
    - Moved the links into 2 partials (signed in) & (non-signed in)
    - Created a  helper method that will help determine which partial to call
    
    JIRA: BEDT-245

---
+++ app/helpers/navigation_helper.rb
@@ -0,0 +1,10 @@
+module NavigationHelper
+
+  def login_and_profile_links_generator
+    if user_signed_in?
+      'layouts/navigation/collapsible_elements/signed_in_links'
+    else
+      'layouts/navigation/collapsible_elements/non_signed_in_links'
+    end
+  end
+end

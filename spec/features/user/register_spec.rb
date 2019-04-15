986f0a4d237d30c2973f685f2e00f49fcc7ece57
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-04-15 16:12:52 +0300

    Add user registration spec
    JIRA BEDT-247

---
+++ spec/features/user/register_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+Rspec.feature "User Registration", :type => :feature do
+  let(:user) { build(:user) }
+
+  scenario 'user navigates to sign up page and successfully signs up', js: true do
+    visit root_path
+    find('nav a', text: 'Register').click
+
+    fill_in 'user[username]', with: user.name
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]' with: user.password_confirmation
+
+    find('.register-buttton').click
+    expect(page).to have_selector('#user-settings')
+
+  end
+end

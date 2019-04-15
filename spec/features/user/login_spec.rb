602a50c64196580ffa01ec104ddf42ccfd3144e6
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-04-15 16:04:57 +0300

    Update login spec
    JIRA: BEDT-247

--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -1,26 +1,17 @@
 require "rails_helper"
 
 RSpec.feature "Login", :type => :feature do
-  before do
-    begin
-      let(:user) { FactoryBot.create(:user) }
-    rescue StandardError => error
-      puts "#{ error.message }"
-    end
-  end
+
+  let(:user) { create :user }
 
   scenario 'user navigates to the login page and successfully logs in', js: true do
-    begin
-      user
-      visit root_path
-      find('nav a', text: 'Login').click
-      fill_in 'user[email]', with: user.email
-      fill_in 'user[password]', with: user.password
-      find('.login-button').click
-      expect(page).to have_selector('#user-settings')
-    rescue StandardError => error
-      puts "#{ error.message }"
-    end
+    user
+    visit root_path
+    find('nav a', text: 'Login').click
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    find('.login-button').click
+    expect(page).to have_selector('#user-settings')
   end
 
 end

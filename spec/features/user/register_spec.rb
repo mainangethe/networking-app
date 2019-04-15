02cc59e621eefb66e450adb0910ba8521e3ab5f3
Ng'ethe Maina <ngethe.maina@tezzasolutions.com>
2019-04-15 16:17:51 +0300

    Update registration spec
    - Syntax errors found
    
    JIRA: BEDT-247

--- spec/features/user/register_spec.rb
+++ spec/features/user/register_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-Rspec.feature "User Registration", :type => :feature do
+RSpec.feature "User Registration", :type => :feature do
   let(:user) { build(:user) }
 
   scenario 'user navigates to sign up page and successfully signs up', js: true do
@@ -10,7 +10,7 @@ Rspec.feature "User Registration", :type => :feature do
     fill_in 'user[username]', with: user.name
     fill_in 'user[email]', with: user.email
     fill_in 'user[password]', with: user.password
-    fill_in 'user[password_confirmation]' with: user.password_confirmation
+    fill_in 'user[password_confirmation]', with: user.password_confirmation
 
     find('.register-buttton').click
     expect(page).to have_selector('#user-settings')
